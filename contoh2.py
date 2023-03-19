5c1d44d7e2e80a3052544cb6ffcb998423faa7ea
Aay18 <85240527+Aay18@users.noreply.github.com>
2023-03-19 10:08:04 +0700

    Add files via upload

---
+++ contoh2.py
@@ -0,0 +1,10 @@
+class Mahasiswa:
+    def __init__(self, nama, npm):
+        self.nama = nama
+        self.npm = npm 
+    
+    def info(self):
+        print(f"Nama: {self.nama}\nNPM: {self.npm}")
+
+mahasiswaA  = Mahasiswa("Ayu", "123")
+mahasiswaA.info()
